b94fe42b600ab5c4ad131195c9a284b77771e611
Samuel Li <samli@samuelli.net>
2013-03-31 00:47:38 +1100

    replaced the pie chart directive with something much easier to understand (found online).

--- html/js/wala.js
+++ html/js/wala.js
@@ -1,3 +1,9 @@
+// initialise angular after gapi is loaded
+google.setOnLoadCallback(function () {
+    angular.bootstrap(document.body, ['wala']);
+});
+google.load('visualization', '1', {packages:['corechart']});
+
 angular.module('wala.services', []). // module name
     factory(
         'ChartService',             // name of this service
@@ -18,47 +24,84 @@ angular.module('wala.services', []). // module name
 );
 
 angular.module('wala', ['wala.services']).
-    directive('chart',function () {
-        return {
-            scope: {
-                // getting read-only value of attribute "chart-data" on the DOM
-                // Confusing! see http://goo.gl/HPBln
-                chartData: "&chartData"
-            },
-            restrict:'A',
-            link:function ($scope, $elm, $attr) {
-                console.log($scope.chartData());
-                // Create the data table.
-                var data = new google.visualization.DataTable();
-                data.addColumn('string', 'Topping');
-                data.addColumn('number', 'Slices');
-                data.addRows(
-//                    [
-//                        ['Mushrooms', 3],
-//                        ['Onions', 1],
-//                        ['Olives', 1],
-//                        ['Zucchini', 1],
-//                        ['Pepperoni', 2]
-//                    ]
-                    $scope.chartData()
-                );
+    // see http://goo.gl/l83A4 for more info on this directive
+    // it has some useful tips on directive scope.
+    // http://plnkr.co/edit/PYC6m4?p=preview
+    directive('pieChart', function ($timeout) {
+      return {
+        restrict: 'EA',
+        scope: {
+          title:    '@title',
+          width:    '@width',
+          height:   '@height',
+          data:     '=data',
+          selectFn: '&select'
+        },
+        link: function($scope, $elm, $attr) {
 
-                // Set chart options
-                var options = {'title':'How Much Pizza I Ate Last Night',
-                    'width':400,
-                    'height':300};
+          // Create the data table and instantiate the chart
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Label');
+          data.addColumn('number', 'Value');
+          var chart = new google.visualization.PieChart($elm[0]);
 
-                // Instantiate and draw our chart, passing in some options.
-                var chart = new google.visualization.PieChart($elm[0]);
-                chart.draw(data, options);
+          draw();
+
+          // Watches, to refresh the chart when its data, title or dimensions change
+          $scope.$watch('data', function() {
+            draw();
+          }, true); // true is for deep object equality checking
+          $scope.$watch('title', function() {
+            draw();
+          });
+          $scope.$watch('width', function() {
+            draw();
+          });
+          $scope.$watch('height', function() {
+            draw();
+          });
+
+          // Chart selection handler (handles event when user select a section of the pie chart)
+          google.visualization.events.addListener(chart, 'select', function () {
+            var selectedItem = chart.getSelection()[0];
+            if (selectedItem) {
+              $scope.$apply(function () {
+                $scope.selectFn({selectedRowIndex: selectedItem.row});
+              });
             }
+          });
+
+          function draw() {
+            if (!draw.triggered) {
+              draw.triggered = true;
+              $timeout(function () {
+                draw.triggered = false;
+                var label, value;
+                data.removeRows(0, data.getNumberOfRows());
+                angular.forEach($scope.data, function(row) {
+                  label = row[0];
+                  value = parseFloat(row[1], 10);
+                  if (!isNaN(value)) {
+                    data.addRow([row[0], value]);
+                  }
+                });
+                var options = {'title': $scope.title,
+                               'width': $scope.width,
+                               'height': $scope.height};
+                chart.draw(data, options);
+                // No raw selected
+                $scope.selectFn({selectedRowIndex: undefined});
+              }, 0, true);
+            }
+          }
         }
+      };
     }).
     // this controller depends on ChartService, which is defined in wala.services module
     // the dependencies are then passed to the controller function
     controller('PieChartCtrl', ['$scope', 'ChartService', function ($scope, chartService) {
         var serviceUrl = 'http://localhost:4567';
-        $scope.counts = [];
+        $scope.chartData = [];
 
         $scope.getRequestTypeData = function (db, coll) {
             console.log('calling getRequestTypeData()');
@@ -68,7 +111,7 @@ angular.module('wala', ['wala.services']).
                 // handling closure
                 promise.then(function (requestType) {
                     return function (resp) {
-                        $scope.counts.push([requestType, resp.data]);
+                        $scope.chartData.push([requestType, resp.data]);
                     }
                 }(requestTypes[i]));
             }
@@ -95,8 +138,3 @@ angular.module('wala', ['wala.services']).
 //        }
 //    }
 //}];
-
-//google.setOnLoadCallback(function() {
-//        angular.bootstrap(document.body, ['wala']);
-//    });
-google.load('visualization', '1', {packages: ['corechart']});
