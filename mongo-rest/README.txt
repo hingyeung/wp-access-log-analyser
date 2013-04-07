ff1a1f530a243ef64488e9df6cfd17971e66fc3f
Samuel Li <samli@samuelli.net>
2013-04-07 16:26:38 +1000

    added home-made mongo http server

---
+++ mongo-rest/README.txt
@@ -0,0 +1,4 @@
+To start up this simple mongo-rest server, run:
+ruby server.rb
+
+Couldn't find any mongo http server that does count() easily. That's why I made my own.
---
+++ mongo-rest/server.rb
@@ -0,0 +1,28 @@
+require "sinatra"
+require "mongo"
+require "URI"
+require "json"
+
+include Mongo
+
+before do
+  if request.request_method == 'OPTIONS'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET"
+    response.headers["Access-Control-Allow-Headers"] = "origin, content-type, accept, X-Requested-With"
+
+    halt 200
+  end
+end
+
+get '/count/:db/:coll' do
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  query = JSON.parse(URI.unescape(params['query'])) unless params['query'].nil?
+  db = MongoClient.new("localhost", 27017).db(params['db'])
+  db[params['coll']].find(query).count.to_s
+end
+
+get '/demo/:db/:coll' do
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  "#{params['query'].nil?} #{params['db']} - #{params['coll']} - #{params['query']}"
+end
